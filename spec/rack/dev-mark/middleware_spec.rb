d9e1f703a700da37feabf44b29407d3adb101773
dtaniwaki <daisuketaniwaki@gmail.com>
2014-06-15 01:20:12 +0900

    Refactoring spec test

--- spec/rack/dev-mark/middleware_spec.rb
+++ spec/rack/dev-mark/middleware_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 describe Rack::DevMark::Middleware do
   let(:headers) { {'Content-Type' => 'text/html; charset=utf-8'} }
   let(:body) { ['response'] }
+  let(:theme) { Class.new(Rack::DevMark::Theme::Base).new }
   let(:app) { double call: [200, headers, body] }
-  subject { Rack::DevMark::Middleware.new(app) }
+  subject { Rack::DevMark::Middleware.new(app, theme) }
   before do
     allow(Rack::DevMark).to receive(:env).and_return('test')
     allow(Rack::DevMark).to receive(:revision).and_return('rev')
+    allow_any_instance_of(Rack::DevMark::Title).to receive(:insert_into){ |body, _, _| "title #{body}" }
+    allow(theme).to receive(:insert_into){ |body, _, _| "#{body} dev-mark" }
   end
 
   it "inserts dev mark" do
-    expect_any_instance_of(Rack::DevMark::Title).to receive(:insert_into){ |body, _, _| "title #{body}" }.once
-    expect_any_instance_of(Rack::DevMark::Theme::GithubForkRibbon).to receive(:insert_into){ |body, _, _| "#{body} dev-mark" }.once
     status, headers, body = subject.call({})
     expect(status).to eq(200)
     expect(headers).to include('Content-Type' => 'text/html; charset=utf-8')
@@ -21,6 +22,13 @@ describe Rack::DevMark::Middleware do
   it "adds http headers" do
     _, headers, _ = subject.call({})
     expect(headers).to include('X-Rack-Dev-Mark-Env' => 'test')
+  end
+  context "default theme" do
+    subject { Rack::DevMark::Middleware.new(app) }
+    it "uses github_fork_ribbon" do
+      expect_any_instance_of(Rack::DevMark::Theme::GithubForkRibbon).to receive(:insert_into).and_return('')
+      subject.call({})
+    end
   end
   context "not html request" do
     let(:headers) { {'Content-Type' => 'application/json;'} }
