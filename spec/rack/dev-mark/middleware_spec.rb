05ae8bf041fb4a63c053a22dbf7a00c52352bb56
dtaniwaki <daisuketaniwaki@gmail.com>
2014-06-15 01:08:09 +0900

    Refactoring spec test

--- spec/rack/dev-mark/middleware_spec.rb
+++ spec/rack/dev-mark/middleware_spec.rb
@@ -1,29 +1,32 @@
 require 'spec_helper'
 
 describe Rack::DevMark::Middleware do
-  let(:app) { double call: [200, {'Content-Type' => 'text/html; charset=utf-8'}, ['response']] }
+  let(:headers) { {'Content-Type' => 'text/html; charset=utf-8'} }
+  let(:body) { ['response'] }
+  let(:app) { double call: [200, headers, body] }
   subject { Rack::DevMark::Middleware.new(app) }
   before do
     allow(Rack::DevMark).to receive(:env).and_return('test')
     allow(Rack::DevMark).to receive(:revision).and_return('rev')
   end
 
-  it "inserts planbcd tag" do
-    expect_any_instance_of(Rack::DevMark::Title).to receive(:insert_into).with('response', 'test', 'rev').once.and_return('response')
-    expect_any_instance_of(Rack::DevMark::Theme::GithubForkRibbon).to receive(:insert_into).with('response', 'test', 'rev').once.and_return('response')
+  it "inserts dev mark" do
+    expect_any_instance_of(Rack::DevMark::Title).to receive(:insert_into){ |body, _, _| "title #{body}" }.once
+    expect_any_instance_of(Rack::DevMark::Theme::GithubForkRibbon).to receive(:insert_into){ |body, _, _| "#{body} dev-mark" }.once
     status, headers, body = subject.call({})
     expect(status).to eq(200)
-    expect(headers).to eq({'Content-Type' => 'text/html; charset=utf-8'})
-    expect(body).to eq(['response'])
+    expect(headers).to include('Content-Type' => 'text/html; charset=utf-8')
+    expect(body).to eq(["title response dev-mark"])
   end
   context "not html request" do
-    let(:app) { double call: [200, {'Content-Type' => 'application/json;'}, ['{}']] }
+    let(:headers) { {'Content-Type' => 'application/json;'} }
+    let(:body) { ['{}'] }
     it "does not insert planbcd tag if the body does not have head tag" do
       expect_any_instance_of(Rack::DevMark::Title).not_to receive(:insert_into)
       expect_any_instance_of(Rack::DevMark::Theme::GithubForkRibbon).not_to receive(:insert_into)
       status, headers, body = subject.call({})
       expect(status).to eq(200)
-      expect(headers).to eq({'Content-Type' => 'application/json;'})
+      expect(headers).to include('Content-Type' => 'application/json;')
       expect(body).to eq(['{}'])
     end
   end
