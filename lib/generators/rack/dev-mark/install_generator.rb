1528a06a84d753d0062aea9f6c355f28439b50f1
dtaniwaki <daisuketaniwaki@gmail.com>
2014-06-21 02:48:06 +0900

    Rename Rails generator

---
+++ lib/generators/rack/dev-mark/install_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators/base'
+
+module Rack
+  module DevMark
+    class InstallGenerator < ::Rails::Generators::Base
+      namespace 'rack:dev-mark:install'
+
+      desc "Install rack-dev-mark."
+      def insert_enable
+        insert_into_file 'config/application.rb', after: "< Rails::Application\n" do <<-EOS
+      config.rack_dev_mark.enable = !Rails.env.production?
+
+        EOS
+        end
+      end
+    end
+  end
+end
