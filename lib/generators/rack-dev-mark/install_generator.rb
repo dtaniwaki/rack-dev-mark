e1f19441582be5b2ef6f779dfb32d91e8efe32d6
dtaniwaki <daisuketaniwaki@gmail.com>
2014-06-21 02:20:55 +0900

    Add Rails generator

---
+++ lib/generators/rack-dev-mark/install_generator.rb
@@ -0,0 +1,16 @@
+require 'rails/generators/base'
+
+module RackDevMark
+  class InstallGenerator < ::Rails::Generators::Base
+    namespace 'rack-dev-mark:install'
+
+    desc "Install rack-dev-mark."
+    def insert_enable
+      insert_into_file 'config/application.rb', after: "< Rails::Application\n" do <<-EOS
+    config.rack_dev_mark.enable = !Rails.env.production?
+
+      EOS
+      end
+    end
+  end
+end
