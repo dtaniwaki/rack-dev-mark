7099e3094726f0deeed5a7356a4c24d52f06e663
dtaniwaki <daisuketaniwaki@gmail.com>
2014-06-21 12:16:12 +0900

    Add more settings in the generator

--- lib/generators/rack/dev-mark/install_generator.rb
+++ lib/generators/rack/dev-mark/install_generator.rb
@@ -8,7 +8,10 @@ module Rack
       desc "Install rack-dev-mark."
       def insert_enable
         insert_into_file 'config/application.rb', after: "< Rails::Application\n" do <<-EOS
+    # Enable rack-dev-mark
     config.rack_dev_mark.enable = !Rails.env.production?
+    # Customize themes if you want to do so
+    # config.rack_dev_mark.custom_theme = [:title, :github_fork_ribbon]
 
         EOS
         end
