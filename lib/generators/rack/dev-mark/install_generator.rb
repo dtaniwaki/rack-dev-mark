173fd0c0d5036dd2a8717c30600ac213514a3846
dtaniwaki <daisuketaniwaki@gmail.com>
2014-07-19 22:56:34 +0900

    Add custom env setting in the rails generator

--- lib/generators/rack/dev-mark/install_generator.rb
+++ lib/generators/rack/dev-mark/install_generator.rb
@@ -11,8 +11,13 @@ module Rack
         insert_into_file target_path, after: "< Rails::Application\n" do <<-EOS
     # Enable rack-dev-mark
     config.rack_dev_mark.enable = !Rails.env.production?
+    #
+    # Customize the env string (default Rails.env)
+    # config.rack_dev_mark.env = 'foo'
+    #
     # Customize themes if you want to do so
     # config.rack_dev_mark.theme = [:title, :github_fork_ribbon]
+    #
     # Customize inserted place of the middleware if necessary.
     # You can use either `insert_before` or `insert_after`
     # config.rack_dev_mark.insert_before SomeOtherMiddleware
--- spec/generators/rack/dev-mark/install_generator_spec.rb
+++ spec/generators/rack/dev-mark/install_generator_spec.rb
@@ -32,8 +32,13 @@ module MyApp
   class Application < Rails::Application
     # Enable rack-dev-mark
     config.rack_dev_mark.enable = !Rails.env.production?
+    #
+    # Customize the env string (default Rails.env)
+    # config.rack_dev_mark.env = 'foo'
+    #
     # Customize themes if you want to do so
     # config.rack_dev_mark.theme = [:title, :github_fork_ribbon]
+    #
     # Customize inserted place of the middleware if necessary.
     # You can use either `insert_before` or `insert_after`
     # config.rack_dev_mark.insert_before SomeOtherMiddleware
