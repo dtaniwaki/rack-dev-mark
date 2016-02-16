dbe4e6b8ad7740fc337fb24024b533f93cff76f0
dtaniwaki <daisuketaniwaki@gmail.com>
2016-02-16 11:44:16 -0800

    Revert "Style tag in head"

--- spec/rack/dev-mark/theme/github_fork_ribbon_spec.rb
+++ spec/rack/dev-mark/theme/github_fork_ribbon_spec.rb
@@ -5,10 +5,11 @@ describe Rack::DevMark::Theme::GithubForkRibbon do
   it_behaves_like "theme" do
     let :out do
       s = <<-EOS
-<html><head>head<title>title</title><style>#{read_stylesheet 'github-fork-ribbon-css/gh-fork-ribbon.css'}</style>
+<html><head>head<title>title</title></head><body><style>#{read_stylesheet "github-fork-ribbon-css/gh-fork-ribbon.css"}</style>
 <!--[if lt IE 9]>
-<style>#{read_stylesheet 'github-fork-ribbon-css/gh-fork-ribbon.ie.css'}</style>
-<![endif]--></head><body><div class="github-fork-ribbon-wrapper left" onClick="this.style.display='none'" title="rev&#10;time"><div class="github-fork-ribbon red"><span class="github-fork-ribbon-text">env</span></div></div>body</body></html>
+<style>#{read_stylesheet "github-fork-ribbon-css/gh-fork-ribbon.ie.css"}</style>
+<![endif]-->
+<div class="github-fork-ribbon-wrapper left" onClick="this.style.display='none'" title="rev&#10;time"><div class="github-fork-ribbon red"><span class="github-fork-ribbon-text">env</span></div></div>body</body></html>
       EOS
       s.strip
     end
@@ -19,10 +20,11 @@ describe Rack::DevMark::Theme::GithubForkRibbon do
     it_behaves_like "theme" do
       let :out do
         s = <<-EOS
-<html><head>head<title>title</title><style>#{read_stylesheet 'github-fork-ribbon-css/gh-fork-ribbon.css'}</style>
+<html><head>head<title>title</title></head><body><style>#{read_stylesheet "github-fork-ribbon-css/gh-fork-ribbon.css"}</style>
 <!--[if lt IE 9]>
-<style>#{read_stylesheet 'github-fork-ribbon-css/gh-fork-ribbon.ie.css'}</style>
-<![endif]--></head><body><div class="github-fork-ribbon-wrapper right" onClick="this.style.display='none'" title="rev&#10;time"><div class="github-fork-ribbon red"><span class="github-fork-ribbon-text">env</span></div></div>body</body></html>
+<style>#{read_stylesheet "github-fork-ribbon-css/gh-fork-ribbon.ie.css"}</style>
+<![endif]-->
+<div class="github-fork-ribbon-wrapper right" onClick="this.style.display='none'" title="rev&#10;time"><div class="github-fork-ribbon red"><span class="github-fork-ribbon-text">env</span></div></div>body</body></html>
         EOS
         s.strip
       end
@@ -34,10 +36,11 @@ describe Rack::DevMark::Theme::GithubForkRibbon do
     it_behaves_like "theme" do
       let :out do
         s = <<-EOS
-<html><head>head<title>title</title><style>#{read_stylesheet 'github-fork-ribbon-css/gh-fork-ribbon.css'}</style>
+<html><head>head<title>title</title></head><body><style>#{read_stylesheet "github-fork-ribbon-css/gh-fork-ribbon.css"}</style>
 <!--[if lt IE 9]>
-<style>#{read_stylesheet 'github-fork-ribbon-css/gh-fork-ribbon.ie.css'}</style>
-<![endif]--></head><body><div class="github-fork-ribbon-wrapper left" onClick="this.style.display='none'" title="rev&#10;time"><div class="github-fork-ribbon orange"><span class="github-fork-ribbon-text">env</span></div></div>body</body></html>
+<style>#{read_stylesheet "github-fork-ribbon-css/gh-fork-ribbon.ie.css"}</style>
+<![endif]-->
+<div class="github-fork-ribbon-wrapper left" onClick="this.style.display='none'" title="rev&#10;time"><div class="github-fork-ribbon orange"><span class="github-fork-ribbon-text">env</span></div></div>body</body></html>
         EOS
         s.strip
       end
@@ -49,10 +52,11 @@ describe Rack::DevMark::Theme::GithubForkRibbon do
     it_behaves_like "theme" do
       let :out do
         s = <<-EOS
-<html><head>head<title>title</title><style>#{read_stylesheet 'github-fork-ribbon-css/gh-fork-ribbon.css'}</style>
+<html><head>head<title>title</title></head><body><style>#{read_stylesheet "github-fork-ribbon-css/gh-fork-ribbon.css"}</style>
 <!--[if lt IE 9]>
-<style>#{read_stylesheet 'github-fork-ribbon-css/gh-fork-ribbon.ie.css'}</style>
-<![endif]--></head><body><div class="github-fork-ribbon-wrapper left fixed" onClick="this.style.display='none'" title="rev&#10;time"><div class="github-fork-ribbon red"><span class="github-fork-ribbon-text">env</span></div></div>body</body></html>
+<style>#{read_stylesheet "github-fork-ribbon-css/gh-fork-ribbon.ie.css"}</style>
+<![endif]-->
+<div class="github-fork-ribbon-wrapper left fixed" onClick="this.style.display='none'" title="rev&#10;time"><div class="github-fork-ribbon red"><span class="github-fork-ribbon-text">env</span></div></div>body</body></html>
         EOS
         s.strip
       end
